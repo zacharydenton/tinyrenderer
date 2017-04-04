70901fb4fe9b035fd8aee38bc4d90cfa393da563
Zach Denton <z@chdenton.com>
2017-04-03 23:04:32 -0700

    Filled triangles.

--- main.cpp
+++ main.cpp
@@ -115,11 +115,31 @@ void wu_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
   }
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+Vec3f barycentric(vector<Vec2i> pts, Vec2i P) { 
+  Vec3f u = Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x) ^ Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y); 
+  if (abs(u.z)<1) return Vec3f(-1,1,1); // triangle is degenerate, in this case return smth with negative coordinates 
+  return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
+} 
+
+void triangle(vector<Vec2i> pts, TGAImage &image, TGAColor color)
 { 
-  bresenham_line(t0, t1, image, color); 
-  bresenham_line(t1, t2, image, color); 
-  bresenham_line(t2, t0, image, color); 
+  Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+  Vec2i bboxmax(0, 0);
+  Vec2i clamp(image.get_width() - 1, image.get_height() - 1); 
+  for (auto i=0; i<3; i++) { 
+    bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x)); 
+    bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x)); 
+    bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y)); 
+    bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y)); 
+  } 
+  Vec2i P; 
+  for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) { 
+    for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) { 
+      Vec3f bc_screen  = barycentric(pts, P); 
+      if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue; 
+      image.set(P.x, P.y, color); 
+    } 
+  } 
 }
 
 void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color)
@@ -147,7 +167,9 @@ void draw_model(const string filename, TGAImage &image)
 int main(int argc, char** argv)
 {
   TGAImage image(width, height, TGAImage::RGB);
-  draw_model("obj/african_head.obj", image);
+  //draw_model("obj/african_head.obj", image);
+  vector<Vec2i> pts = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)}; 
+  triangle(pts, image, red);
   image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
